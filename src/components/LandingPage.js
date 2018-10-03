a15e5074f2cd2bee50095d1455720743dd53c4fe
Ossiggy <mrossig201@gmail.com>
2018-10-03 11:50:50 -0400

    Landing page updated to include SignUpForm and secondary jumborton

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,17 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-import {Container, Row, Col, Jumbotron} from 'reactstrap';
+import {Jumbotron} from 'reactstrap';
 
 import '../css/LandingPage.css'
 
 export default function LandingPage(props) {
   return (
     <div className='lander'>
-      <Jumbotron className="jumbo-intro">
+      <Jumbotron className="jumbo-top">
         <h1 className='jumbo-title'>Track your mind</h1>
         <p className='display-5'>Shower ideas, epiphanies, moments of clarity, all of it</p>
         <hr/>
-        <p className='lead'>Never again forget the hard earned lessons you acquire daily</p>
+        <p className='lead'>Never forget the hard earned lessons you acquire daily</p>
+      </Jumbotron>
+
+      <Jumbotron className="jumbo-bottom">
+        <h1 className='jumbo-title'>Access Anywhere</h1>
+        <p className='display-5'>Keep your journal with you on the go</p>
+        <hr/>
+        <p className='lead'>Have the information ready when you need it most</p>
       </Jumbotron>
     </div>
   );
