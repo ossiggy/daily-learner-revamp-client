cce5b1680c912bd81d0b7f8b0051f632926683c5
Ossiggy <mrossig201@gmail.com>
2018-10-03 11:51:11 -0400

    SignUpForm basic structure completed

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,2 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 
+import {Button, Form, FormGroup, Label, Input, Jumbotron, Container, Row, Col} from 'reactstrap';
+
+import "../css/SignUpForm.css";
+
+export default function SignUpForm(props){
+  return(
+    <Jumbotron className="sign-up-form">
+      <h1 className="sign-up-title">Sign Up</h1>
+      <Form>
+        <FormGroup>
+          <Label for="sign-up-email">Email</Label>
+          <Input type="email" name="email" id="sign-up-email" placeholder="example@example.com"/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="sign-up-password">Password</Label>
+          <Input type="password" name="password" id="sign-up-password" placeholder="*******"/>
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    </Jumbotron>
+  )
+}
