52517618eae25b58ea8539a7e164e49be4dec145
Ossiggy <mrossig201@gmail.com>
2018-10-03 11:49:29 -0400

    landing page rendered through Dashboard component, Dashboard component rendered by App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import Header from './components/Header';
-import LandingPage from './components/LandingPage';
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <LandingPage />
+        <Dashboard />
       </div>
     );
   }
