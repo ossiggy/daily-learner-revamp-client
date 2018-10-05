803731e9357a16dd0990772c33820cd4e1b216a1
Ossiggy <mrossig201@gmail.com>
2018-10-04 23:04:14 -0400

    added react-router-dom and set routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import {Route, withRouter} from 'react-router-dom';
 
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
+import LandingPage from './components/LandingPage';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Dashboard />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/about" component={About} />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
