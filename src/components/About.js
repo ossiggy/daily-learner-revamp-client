f753838f6b8487484b18daf9aa61139068a69dbc
Ossiggy <mrossig201@gmail.com>
2018-10-19 18:07:31 -0400

    copied over 'About' information from orignal site

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,31 @@ import '../css/About.css';
 
 export default function About(props) {
   return (
-    <div>
-      <Jumbotron className="jumbo-about">
+    <div className="about">
+
+      <Jumbotron className="jumbo-about-title">
         <h1 className='display-3'>The last journal you'll ever need</h1>
       </Jumbotron>
+
+      <Jumbotron className="jumbo-top">
+        <h1 className="display-5"><i>"You learn something new every day"</i></h1>
+        <hr/>
+        <p>
+          Imagine how much you could benefit from having a collection 
+          of all that information on tap and ready to be accessed?
+        </p>
+      </Jumbotron>
+
+      <Jumbotron className="jumbo-bottom">
+        <h1 className="display-5"><i>Enter: Daily Learner</i></h1>
+        <hr/>
+        <p>
+          This app was designed specifically to keep track of your hard earned
+          life lessons and organize them into an easily searchable database.  Just write down your lesson,
+          add an applicable category, and voila! You can now track all the knowledge you gain on a daily basis.
+        </p>
+      </Jumbotron>
+
     </div>
   );
 }
