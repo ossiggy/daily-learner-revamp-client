ca23fe158f2f6541883ba59a8da8abefb6036783
Ossiggy <mrossig201@gmail.com>
2018-10-23 21:45:58 -0400

    laid groundwork for article viewer component

---
+++ src/components/ArticleViewer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import {Link, Redirect} from 'react-router-dom';
+// import {connect} from 'react-redux';
+// import {fetchAllArticles, deleteArticle} from '../actions/';
+
+export class ArticleViewer extends Component{
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
