7e0851172b61642aa3d64e5dc42452a8cea91f96
Ossiggy <mrossig201@gmail.com>
2018-10-23 21:40:51 -0400

    added paths for lesson components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import LandingPage from './components/LandingPage';
 import About from './components/About';
+import { ArticleForm } from './components/ArticleForm';
+import {ArticleViewer} from './components/ArticleViewer';
 
 class App extends Component {
   render() {
@@ -14,6 +16,8 @@ class App extends Component {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/about" component={About} />
+        <Route exact path="new-lesson" component={ArticleForm} />
+        <Route exact path="/all-lessons" component={ArticleViewer} />
       </div>
     );
   }
