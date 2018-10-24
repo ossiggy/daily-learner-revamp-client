a0e756af8299f74f06d92c2976cc819d17714342
Ossiggy <mrossig201@gmail.com>
2018-10-23 21:43:09 -0400

    changed classnames on Jumbotrons for styling

--- src/components/About.js
+++ src/components/About.js
@@ -12,19 +12,19 @@ export default function About(props) {
         <h1 className='display-3'>The last journal you'll ever need</h1>
       </Jumbotron>
 
-      <Jumbotron className="jumbo-top">
+      <Jumbotron className="jumbo-about-top">
         <h1 className="display-5"><i>"You learn something new every day"</i></h1>
         <hr/>
-        <p>
+        <p className="about-text">
           Imagine how much you could benefit from having a collection 
           of all that information on tap and ready to be accessed?
         </p>
       </Jumbotron>
 
-      <Jumbotron className="jumbo-bottom">
+      <Jumbotron className="jumbo-about-bottom">
         <h1 className="display-5"><i>Enter: Daily Learner</i></h1>
         <hr/>
-        <p>
+        <p className="about-text">
           This app was designed specifically to keep track of your hard earned
           life lessons and organize them into an easily searchable database.  Just write down your lesson,
           add an applicable category, and voila! You can now track all the knowledge you gain on a daily basis.
