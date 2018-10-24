9094c5f96b019cf60d596e2570bd344b18dd0a12
Ossiggy <mrossig201@gmail.com>
2018-10-23 21:44:38 -0400

    made the header dynamic to user login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {menuToggle, logout} from '../actions';
 
 import {
   Button,
+  ButtonDropdown, 
+  DropdownToggle, 
+  DropdownMenu, 
+  DropdownItem,
   Navbar,
   NavbarBrand,
   Nav,
@@ -13,32 +20,75 @@ import image from '../assets/logo.png';
 
 import '../css/Header.css'
 
-export default class Header extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
+export function Header(props){
 
-    }
+  let navi
+
+  if(!props.loggedIn){
+    navi = (
+      <Nav>
+        <NavItem>
+          <NavLink href="/about">
+            About
+          </NavLink>
+        </NavItem>
+        <Button outline color="secondary">Log In</Button>
+        </Nav>
+    )
   }
 
-  render(){
-    return(
+  if(props.loggedIn){
+    navi = (
+      <Nav>
+        <NavItem>
+          <ButtonDropdown 
+          isOpen={props.menuOpen} 
+          toggle={() => props.dispatch(menuToggle())}
+          direction="left"
+          >
+            <DropdownToggle caret>Menu</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>
+                <NavLink href="/articles">
+                  Articles
+                </NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink  href="/articleform">
+                  New Article
+                </NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink className="logout" onClick={()=>props.dispatch(logout())}>
+                  Log Out
+                </NavLink>
+              </DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </NavItem>
+      </Nav>
+    )
+  }
+
+  return(
     <div>
       <Navbar color='faded'>
         <NavbarBrand href="/"><img alt="logo" src={image}/></NavbarBrand>
-          <Nav>
-            <NavItem>
-              <NavLink href="/about">
-                About
-              </NavLink>
-            </NavItem>
-            <Button outline color="secondary">Log In</Button>
-          </Nav>
+          {navi}
       </Navbar>
     </div>
 
     // transition in the signup form
     // Photo by Easton Oliver on Unsplash
     );
-  }
 }
+
+const mapStateToProps = state => {
+  return {
+    menuOpen: state.menu.menuOpen,
+    loggedIn: true,
+    currentUser: state.auth.currentUser
+  };
+};
+
+export default connect(mapStateToProps)(Header);
